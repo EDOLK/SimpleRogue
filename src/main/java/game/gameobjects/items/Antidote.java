aaa1cc67d4950b4dbca15be4ad64c07ac1a70508
Edward <edwardolkh@gmail.com>
2025-10-27 12:56:40 -0400

    bandage and antidote

--- src/main/java/game/gameobjects/items/Antidote.java
+++ src/main/java/game/gameobjects/items/Antidote.java
@@ -2,18 +2,17 @@ package game.gameobjects.items;
 
 import org.hexworks.zircon.api.color.TileColor;
 
-import game.display.Display;
+import game.App;
 import game.gamelogic.Consumable;
 import game.gameobjects.entities.Entity;
-import game.gameobjects.entities.PlayerEntity;
-import game.gameobjects.statuses.Poisoned;
+import game.gameobjects.statuses.PoisonImmune;
 
 public class Antidote extends Item implements Consumable {
 
     public Antidote() {
         super();
         setName("Antidote");
-        setDescription("A small green vial of bitter medicine.");
+        setDescription("A small vial of bitter green medicine.");
         setCharacter('a');
         setWeight(1);
         setBgColor(TileColor.transparent());
@@ -22,15 +21,8 @@ public class Antidote extends Item implements Consumable {
 
     @Override
     public boolean consume(Entity consumer) {
-        Poisoned poisoned = (Poisoned)consumer.getStatusByClass(Poisoned.class);
-        if (poisoned != null) {
-            consumer.removeStatus(poisoned);
-            if (consumer instanceof PlayerEntity) {
-                Display.log("You feel better.");
-            }
-            return true;
-        }
-        return false;
+        consumer.addStatus(new PoisonImmune(App.randomNumber(5, 15)));
+        return true;
     }
 
     
