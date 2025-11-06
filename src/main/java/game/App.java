12cc916a3de6637a9f51ad1b06d73973b7aa65d2
Edward <edwardolkh@gmail.com>
2025-11-06 18:08:33 -0500

    woops

--- src/main/java/game/App.java
+++ src/main/java/game/App.java
@@ -18,7 +18,6 @@ import game.gamelogic.abilities.HasAbility;
 import game.gamelogic.abilities.HasPassives;
 import game.gameobjects.entities.Entity;
 import game.gameobjects.items.armor.Armor;
-import game.gameobjects.items.potions.FirePotion;
 import game.gameobjects.items.weapons.Weapon;
 
 public class App 
@@ -26,7 +25,6 @@ public class App
     public static void main(String[] args){
         Dungeon.initialize(50, 50);
         Display.initialize(70, 61);
-        Dungeon.getCurrentFloor().getPlayer().addItemToInventory(new FirePotion());
     }
 
     public static int randomNumber(int min, int max){
