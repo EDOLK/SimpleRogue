3eb01a5ee996dfc4fc945bd44522f25108c94794
EDOLK <119468544+EDOLK@users.noreply.github.com>
2025-11-28 15:05:36 -0500

    Complete MultiSkillEntryBuilder refactor (#85)

--- src/main/java/game/gamelogic/skilltrees/MultiSkillEntryBuilder.java
+++ src/main/java/game/gamelogic/skilltrees/MultiSkillEntryBuilder.java
@@ -1,61 +1,13 @@
 package game.gamelogic.skilltrees;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
-import game.gamelogic.Attribute;
-import game.gamelogic.Skill;
-import game.gamelogic.abilities.Ability;
-import game.gameobjects.entities.Entity;
-import kotlin.Pair;
-
 public class MultiSkillEntryBuilder {
-    private Function<Integer, String> namingStrategy = (i) -> "Placeholder name";
-    private Function<Integer, String> descriptionStrategy = (i) -> "Placeholder description";
-    private Collection<Function<Integer, Pair<Ability, Integer>>> abilityRequirementStrategy = new ArrayList<>();
-    private Collection<Function<Integer, Pair<Attribute, Integer>>> attributeRequirementStrategy = new ArrayList<>();
-    private Collection<Function<Integer, Pair<SkillTree, Integer>>> skillTreeRequirementStrategy = new ArrayList<>();
-    private Collection<Function<Integer, Pair<Skill, Integer>>> skillRequirementStrategy = new ArrayList<>();
-    private Collection<Function<Integer, Consumer<Entity>>> applyFunctionStrategy = new ArrayList<>();
-    private Function<Integer, Integer> levelRequirementStrategy = (i) -> -1;
-    private Function<Integer, Integer> costStrategy = (i) -> 0;
 
-    public MultiSkillEntryBuilder setNamingStrategy(Function<Integer, String> namingStrategy) {
-        this.namingStrategy = namingStrategy;
-        return this;
-    }
-    public MultiSkillEntryBuilder setDescriptionStrategy(Function<Integer, String> descriptionStrategy) {
-        this.descriptionStrategy = descriptionStrategy;
-        return this;
-    }
-    public MultiSkillEntryBuilder addAbilityRequirementStrategy(Function<Integer, Pair<Ability, Integer>> abilityRequirementStrategy) {
-        this.abilityRequirementStrategy.add(abilityRequirementStrategy);
-        return this;
-    }
-    public MultiSkillEntryBuilder addAttributeRequirementStrategy(Function<Integer, Pair<Attribute, Integer>> attributeRequirementStrategy) {
-        this.attributeRequirementStrategy.add(attributeRequirementStrategy);
-        return this;
-    }
-    public MultiSkillEntryBuilder addSkillTreeRequirementStrategy(Function<Integer, Pair<SkillTree, Integer>> skillTreeRequirementStrategy) {
-        this.skillTreeRequirementStrategy.add(skillTreeRequirementStrategy);
-        return this;
-    }
-    public MultiSkillEntryBuilder addSkillRequirementStrategy(Function<Integer, Pair<Skill, Integer>> skillRequirementStrategy) {
-        this.skillRequirementStrategy.add(skillRequirementStrategy);
-        return this;
-    }
-    public MultiSkillEntryBuilder addApplyFunctionStrategy(Function<Integer, Consumer<Entity>> applyFunctionStrategy) {
-        this.applyFunctionStrategy.add(applyFunctionStrategy);
-        return this;
-    }
-    public MultiSkillEntryBuilder setLevelRequirementStrategy(Function<Integer, Integer> levelRequirementStrategy) {
-        this.levelRequirementStrategy = levelRequirementStrategy;
-        return this;
-    }
-    public MultiSkillEntryBuilder setCostStrategy(Function<Integer, Integer> costStrategy) {
-        this.costStrategy = costStrategy;
+    private Function<Integer, SkillEntryBuilder> strategy;
+
+    public MultiSkillEntryBuilder withStrategy(Function<Integer, SkillEntryBuilder> strategy) {
+        this.strategy = strategy;
         return this;
     }
 
@@ -94,33 +46,11 @@ public class MultiSkillEntryBuilder {
         SkillEntryBuilder[] entries = new SkillEntryBuilder[amount];
         int j = 0;
         for (int i = from; i != (from > to ? to-1 : to+1); i += (from > to) ? -1 : 1) {
-            SkillEntryBuilder builder = new SkillEntryBuilder()
-                .addName(this.namingStrategy.apply(i))
-                .addDescription(this.descriptionStrategy.apply(i))
-                .addLevelRequirement(this.levelRequirementStrategy.apply(i))
-                .addCostRequirement(this.costStrategy.apply(i));
-            for (Function<Integer, Pair<Attribute, Integer>> func : attributeRequirementStrategy) {
-                Pair<Attribute, Integer> entry = func.apply(i);
-                builder = builder.addAttributeRequirement(entry.getFirst(), entry.getSecond());
-            }
-            for (Function<Integer, Pair<Ability, Integer>> func : abilityRequirementStrategy) {
-                Pair<Ability, Integer> entry = func.apply(i);
-                builder = builder.addAbilityRequirement(entry.getFirst(), entry.getSecond());
-            }
-            for (Function<Integer, Pair<Skill, Integer>> func : skillRequirementStrategy) {
-                Pair<Skill, Integer> entry = func.apply(i);
-                builder = builder.addSkillRequirement(entry.getFirst(), entry.getSecond());
-            }
-            for (Function<Integer, Pair<SkillTree, Integer>> func : skillTreeRequirementStrategy) {
-                Pair<SkillTree, Integer> entry = func.apply(i);
-                builder = builder.addSkillTreeRequirement(entry.getFirst(), entry.getSecond());
-            }
-            for (Function<Integer, Consumer<Entity>> func : applyFunctionStrategy) {
-                builder = builder.addOnApply(func.apply(i));
-            }
+            SkillEntryBuilder builder = strategy.apply(i);
             entries[j] = builder;
             j++;
         }
         return entries;
     }
+
 }
--- src/main/java/game/gamelogic/skilltrees/warrior/WarriorSkillTree.java
+++ src/main/java/game/gamelogic/skilltrees/warrior/WarriorSkillTree.java
@@ -7,7 +7,6 @@ import game.gamelogic.abilities.HasAbilities;
 import game.gamelogic.skilltrees.MultiSkillEntryBuilder;
 import game.gamelogic.skilltrees.SkillEntryBuilder;
 import game.gamelogic.skilltrees.SkillTree;
-import kotlin.Pair;
 
 public class WarriorSkillTree extends SkillTree {
 
@@ -42,55 +41,48 @@ public class WarriorSkillTree extends SkillTree {
 
         addSkillEntry(
             new MultiSkillEntryBuilder()
-                .setNamingStrategy((i) -> {
+                .withStrategy((i) -> {
                     String str = "Bulwark ";
                     for (int j = 0; j < i; j++) {
                         str += "I";
                     }
-                    return str;
-                })
-                .setCostStrategy((i) -> i)
-                .addAttributeRequirementStrategy((i) -> {
-                    return new Pair<Attribute, Integer>(Attribute.ENDURANCE, i <= 2 ? 1 : 2);
-                })
-                .addApplyFunctionStrategy((i) -> {
-                    return (e) -> {
-                        if (e instanceof HasAbilities hasAbilities){
-                            Optional<Bulwark> bulwark = hasAbilities.getAbilityByClass(Bulwark.class);
-                            if (bulwark.isPresent()) {
-                                bulwark.get().setLevel(i);
-                            } else {
-                                hasAbilities.addAbility(new Bulwark(e));
+                    return new SkillEntryBuilder()
+                        .addName(str)
+                        .addAttributeRequirement(Attribute.ENDURANCE, i <= 2 ? 1 : 2)
+                        .addOnApply((e) -> {
+                            if (e instanceof HasAbilities hasAbilities){
+                                Optional<Bulwark> bulwark = hasAbilities.getAbilityByClass(Bulwark.class);
+                                if (bulwark.isPresent()) {
+                                    bulwark.get().setLevel(i);
+                                } else {
+                                    hasAbilities.addAbility(new Bulwark(e));
+                                }
                             }
-                        }
-                    };
+                        });
                 }).chainBuild(this, 1, 3).build()
         );
 
         addSkillEntry(
             new MultiSkillEntryBuilder()
-                .setNamingStrategy((i) -> {
+                .withStrategy((i) -> {
                     String str = "Reliable Combatant ";
                     for (int j = 0; j < i; j++) {
                         str += "I";
                     }
-                    return str;
-                })
-                .setCostStrategy((i) -> i)
-                .addAttributeRequirementStrategy((i) -> {
-                    return new Pair<Attribute, Integer>(Attribute.DEXTERITY, i <= 2 ? 1 : 2);
-                })
-                .addApplyFunctionStrategy((i) -> {
-                    return (e) -> {
-                        if (e instanceof HasAbilities hasAbilities){
-                            Optional<ReliableCombatant> reliableCombatant = hasAbilities.getAbilityByClass(ReliableCombatant.class);
-                            if (reliableCombatant.isPresent()) {
-                                reliableCombatant.get().setLevel(i);
-                            } else {
-                                hasAbilities.addAbility(new ReliableCombatant(e));
+                    return new SkillEntryBuilder()
+                        .addName(str)
+                        .addCostRequirement(i)
+                        .addAttributeRequirement(Attribute.DEXTERITY, i <= 2 ? 1 : 2)
+                        .addOnApply((e) -> {
+                            if (e instanceof HasAbilities hasAbilities){
+                                Optional<ReliableCombatant> reliableCombatant = hasAbilities.getAbilityByClass(ReliableCombatant.class);
+                                if (reliableCombatant.isPresent()) {
+                                    reliableCombatant.get().setLevel(i);
+                                } else {
+                                    hasAbilities.addAbility(new ReliableCombatant(e));
+                                }
                             }
-                        }
-                    };
+                        });
                 }).chainBuild(this, 1, 3).build()
         );
     }
